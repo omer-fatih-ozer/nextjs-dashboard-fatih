71a6bae50eead79526ecefab65399c677641884c
Fatih <omer.fatih.ozer@gmail.com>
2025-02-07 11:50:05 +0100

    Starter code and files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`} />
     </html>
   );
 }
